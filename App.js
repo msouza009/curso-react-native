17aa9e17dc6cbb52a9138ff5ceaf571b11ab7f8c
Matheus <ra23371012-2@aluno.unicesumar.edu.br>
2024-12-13 15:42:18 -0300

    aprendizado com sucesso

--- App.js
+++ App.js
@@ -1,19 +1,60 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      nome: '',
+      input: ''
+    };
+    
+    this.entrar = this.entrar.bind(this);
+  }
+
+  entrar(){
+    if(this.state.input === ''){
+      alert('Digite seu nome!');
+      return;
+    }
+    this.setState({nome: 'Bem vindo: ' + this.state.input});
+  }
+
 
   render() {
-
     return (
-      <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center'}}>
+      <View style={styles.container}>
+        <TextInput 
+        style={styles.input} 
+        placeholder="Digite seu nome:" 
+        underlineColorAndroid="transparent" 
+        onChangeText={(texto => this.setState({input: texto}))}  />
 
-        <View style={{width: 50, height: 50, backgroundColor: 'blue'}}></View>
-        <View style={{width: 50, height: 50, backgroundColor: 'green'}}></View>
-        <View style={{width: 50, height: 50,  backgroundColor: 'yellow'}}></View>
+        <Button title="Entrar" onPress={this.entrar}/>
+
+        <Text style={styles.texto}> {this.state.nome} </Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  input:{
+    height: 45,
+    borderWidth: 1,
+    borderColor: '#222',
+    margin: 10,
+    fontSize: 20,
+    padding: 10,
+  },
+  texto:{
+    textAlign: 'center',
+    fontSize: 25,  
+    color: '#000'
+  }
+});
 
 export default App;
